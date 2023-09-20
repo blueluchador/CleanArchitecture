945a501ec9ba59b5e6e7789232aaf6e597aff1e0
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-20 15:08:50 -0600

    Added custom problem details

---
+++ src/CleanArchitecture.Web/Extensions/ProblemDetailsExtensions.cs
@@ -0,0 +1,46 @@
+using Hellang.Middleware.ProblemDetails;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace CleanArchitecture.Web.Extensions;
+
+public static class ProblemDetailsExtensions
+{
+    public static IServiceCollection AddCustomProblemDetails(this IServiceCollection services)
+    {
+        return services.AddProblemDetails(opt =>
+        {
+            opt.Rethrow<NotSupportedException>();
+            // TODO: What project will this exception come from?
+            //opt.MapToStatusCode<ItemNotFoundException>(StatusCodes.Status404NotFound);
+            opt.MapToStatusCode<NotImplementedException>(StatusCodes.Status501NotImplemented);
+            opt.MapToStatusCode<HttpRequestException>(StatusCodes.Status503ServiceUnavailable);
+            opt.MapToStatusCode<Exception>(StatusCodes.Status500InternalServerError);
+
+            opt.IncludeExceptionDetails = (context, _) =>
+            {
+                var env = context.RequestServices.GetRequiredService<IHostEnvironment>();
+                return env.IsDevelopment() || env.IsStaging();
+            };
+
+            opt.OnBeforeWriteDetails = (context, _) =>
+            {
+                var logger = context.RequestServices.GetService<ILogger<Program>>();
+                if (logger == null)
+                    return;
+                
+                var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                if (contextFeature == null)
+                {
+                    logger.LogError("An unexpected error occured for the request {Url}",
+                        context.Request.GetDisplayUrl());
+                    return;
+                }
+
+                var exception = contextFeature.Error;
+                logger.LogError(exception, "The request at {Url} threw an error with the message: {Message}",
+                    context.Request.GetDisplayUrl(), exception.Message);
+            };
+        });
+    }
+}
--- src/CleanArchitecture.Web/Program.cs
+++ src/CleanArchitecture.Web/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddApiDocs();
 
+builder.Services.AddCustomProblemDetails();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
