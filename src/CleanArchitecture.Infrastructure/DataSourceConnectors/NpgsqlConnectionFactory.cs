c345606cefb5114351b5ef53888a9f88deac99f7
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-22 11:36:44 -0600

    Created PostgreSQL connection factory

---
+++ src/CleanArchitecture.Infrastructure/DataSourceConnectors/NpgsqlConnectionFactory.cs
@@ -0,0 +1,32 @@
+using System.Data;
+using Npgsql;
+
+namespace CleanArchitecture.Infrastructure.DataSourceConnectors;
+
+public class NpgsqlConnectionFactory : IDbConnectionFactory
+{
+    private readonly string _connectionString;
+    
+    public NpgsqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    
+    public IDbConnection CreateConnection()
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        
+        connection.Open();
+
+        return connection;
+    }
+    
+    public async Task<IDbConnection> CreateConnectionAsync()
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        
+        await connection.OpenAsync();
+
+        return connection;
+    }
+}
