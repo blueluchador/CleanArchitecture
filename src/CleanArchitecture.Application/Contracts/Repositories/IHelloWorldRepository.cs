e936b04be049519731826581e10c7fc6b7b0128f
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-22 13:09:58 -0600

    Created GetHelloWorld method stub

--- src/CleanArchitecture.Application/Contracts/Repositories/IHelloWorldRepository.cs
+++ src/CleanArchitecture.Application/Contracts/Repositories/IHelloWorldRepository.cs
@@ -1,6 +1,8 @@
+using CleanArchitecture.Domain.Entities;
+
 namespace CleanArchitecture.Application.Contracts.Repositories;
 
 public interface IHelloWorldRepository
 {
-    
+    Task<HelloWorld> GetHelloWorld(Guid helloWorldId);
 }
--- src/CleanArchitecture.Infrastructure/Repositories/HelloWorldRepository.cs
+++ src/CleanArchitecture.Infrastructure/Repositories/HelloWorldRepository.cs
@@ -1,6 +1,12 @@
+using CleanArchitecture.Application.Contracts.Repositories;
+using CleanArchitecture.Domain.Entities;
+
 namespace CleanArchitecture.Infrastructure.Repositories;
 
-public class HelloWorldRepository
+public class HelloWorldRepository : IHelloWorldRepository
 {
-    
+    public Task<HelloWorld> GetHelloWorld(Guid helloWorldId)
+    {
+        throw new NotImplementedException();
+    }
 }
