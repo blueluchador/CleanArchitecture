abd0f203d60d2936e0c4c2892e608f01207a34dc
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-22 07:18:35 -0600

    Initialize strings with null-forgiving operator

--- src/CleanArchitecture.Domain/Entities/HelloWorld.cs
+++ src/CleanArchitecture.Domain/Entities/HelloWorld.cs
@@ -4,5 +4,5 @@ public class HelloWorld
 {
     public int Id { get; set; }
     public Guid Uuid { get; set; }
-    public string Name { get; set; } = "";
+    public string Name { get; set; } = null!;
 }
