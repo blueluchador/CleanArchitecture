6b148ecc9d5e4369ff1d92fc0a891a859b09b8f1
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-22 22:05:06 -0600

    Added get embeddedResource static method

--- src/CleanArchitecture.Infrastructure/EmbeddedSqlResources/Resource.cs
+++ src/CleanArchitecture.Infrastructure/EmbeddedSqlResources/Resource.cs
@@ -1,6 +1,22 @@
+using System.Reflection;
+
 namespace CleanArchitecture.Infrastructure.EmbeddedSqlResources;
 
-public class Resource
+internal static class Resource
 {
-    
+    private static string GetEmbeddedResource(string fileName)
+    {
+        string filePath = $"CleanArchitecture.Infrastructure.EmbeddedSqlResources.SqlScripts.{fileName}";
+
+        using var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(filePath);
+
+        if (stream == null)
+        {
+            throw new Exception($"The SQL file '{fileName}' was not found.");
+        }
+
+        using var reader = new StreamReader(stream);
+
+        return reader.ReadToEnd();
+    }
 }
