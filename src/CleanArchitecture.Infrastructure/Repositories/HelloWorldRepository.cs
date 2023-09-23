7e8dc261cb13f08b7cf1bb77613276e6bf5beeb5
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-23 15:40:57 -0600

    Implemented hello world repo (no embedded resource)

--- src/CleanArchitecture.Infrastructure/Repositories/HelloWorldRepository.cs
+++ src/CleanArchitecture.Infrastructure/Repositories/HelloWorldRepository.cs
@@ -1,12 +1,31 @@
 using CleanArchitecture.Application.Contracts.Repositories;
 using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Infrastructure.DataSourceConnectors;
+using CleanArchitecture.Infrastructure.EmbeddedSqlResources;
+using Dapper;
+using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.Infrastructure.Repositories;
 
 public class HelloWorldRepository : IHelloWorldRepository
 {
-    public Task<HelloWorld> GetHelloWorld(Guid helloWorldId)
+    private readonly IDbConnectionFactory _connectionFactory;
+    private readonly ILogger<HelloWorldRepository> _logger;
+
+    public HelloWorldRepository(IDbConnectionFactory connectionFactory, ILogger<HelloWorldRepository> logger)
     {
-        throw new NotImplementedException();
+        _connectionFactory = connectionFactory;
+        _logger = logger;
+    }
+
+    public async Task<HelloWorld?> GetHelloWorld(Guid helloWorldId)
+    {
+        _logger.LogInformation("Get hello world, Hello World ID: {HelloWorldID}", helloWorldId);
+        
+        var @params = new { uuid = helloWorldId };
+        
+        using var conn = await _connectionFactory.CreateConnectionAsync();
+        return await conn.QuerySingleOrDefaultAsync<HelloWorld?>("", @params);
+        //return await conn.QuerySingleOrDefaultAsync<HelloWorld?>(Resource.GetHelloWorldQuery, @params);
     }
 }
