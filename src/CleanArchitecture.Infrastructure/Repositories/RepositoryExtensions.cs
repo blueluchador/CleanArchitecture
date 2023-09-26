c226c99363c0e9eb0908700dc76d44c76b2ab64a
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-26 12:26:37 -0600

    Explicitly initialize repo services

--- src/CleanArchitecture.Infrastructure/Repositories/RepositoryExtensions.cs
+++ src/CleanArchitecture.Infrastructure/Repositories/RepositoryExtensions.cs
@@ -2,6 +2,7 @@ using CleanArchitecture.Application.Contracts.Repositories;
 using CleanArchitecture.Infrastructure.DataSourceConnectors;
 using CleanArchitecture.Infrastructure.ORM;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CleanArchitecture.Infrastructure.Repositories;
 
@@ -10,7 +11,8 @@ public static class RepositoryExtensions
     public static IServiceCollection AddHelloWorldRepository(this IServiceCollection services, string connectionString)
     {
         return services
-            .AddSingleton<IObjectMapper>(_ => new HelloWorldObjectMapper(new NpgsqlConnectionFactory(connectionString)))
-            .AddScoped<IHelloWorldRepository, HelloWorldRepository>();
+            .AddScoped<IHelloWorldRepository>(p => new HelloWorldRepository(
+                new HelloWorldObjectMapper(new NpgsqlConnectionFactory(connectionString)),
+                p.GetRequiredService<ILogger<HelloWorldRepository>>()));
     }
 }
