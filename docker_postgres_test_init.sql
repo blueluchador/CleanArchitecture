c9a124312559a024dd71710028804d66412e8152
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-26 21:30:08 -0600

    include not supported in sql scripts

--- docker_postgres_test_init.sql
+++ docker_postgres_test_init.sql
@@ -1,4 +1,14 @@
-EXEC SQL INCLUDE docker_postgres_init.sql;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+
+CREATE table person
+(
+    id serial,
+    uuid uuid NOT NULL DEFAULT gen_random_uuid(),
+    first_name varchar(40) NOT NULL,
+    last_name varchar(40),
+
+    PRIMARY KEY (id)
+);
 
 -- Seed test data
 INSERT INTO person(uuid, first_name, last_name) VALUES ('b5d74ff1-572f-4dd5-beb3-3aa67adf6b49', 'Gerardo', 'Gastelum')
