9465263867abc557ed9db99a7dcc869414e88e06
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-26 22:17:38 -0600

    Fixed integration tests

--- tests/CleanArchitecture.IntegrationTests/ApiEndpoints/PersonsApiTests.cs
+++ tests/CleanArchitecture.IntegrationTests/ApiEndpoints/PersonsApiTests.cs
@@ -26,7 +26,22 @@ public class PersonsApiTests : IClassFixture<CustomApplicationFactory>
         response.Should().HaveStatusCode(HttpStatusCode.OK);
 
         message.Should().NotBeNull("because the endpoint returns any message");
-        message.Should().Be(new HelloWorldMessage { Message = "Hello, Gerardo!" },
-            "because the person table returns 'Gerardo'");
+        message.Message.Should().Be("Hello, Gerardo!", "because the person table returns 'Gerardo'");
+    }
+    
+    [Fact]
+    public async Task GetHelloWorldMessage_ReturnsDefaultMessage()
+    {
+        // Arrange
+
+        // Act
+        var response = await _client.GetAsync("/persons/53b2abec-a06b-4686-9c5a-5ede268eab6b/message");
+        var message = await response.ProcessResponse<HelloWorldMessage>();
+        
+        // Assert
+        response.Should().HaveStatusCode(HttpStatusCode.OK);
+
+        message.Should().NotBeNull("because the endpoint returns any message");
+        message.Message.Should().Be("Hello, World!", "because the person table returns no rows");
     }
 }
