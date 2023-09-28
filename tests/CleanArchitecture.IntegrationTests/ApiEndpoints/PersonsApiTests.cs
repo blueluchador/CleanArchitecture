c83336c3ad296e61f4388bad6fb6d0cd05beeecc
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-28 16:42:53 -0600

    Refactored unit test

--- tests/CleanArchitecture.IntegrationTests/ApiEndpoints/PersonsApiTests.cs
+++ tests/CleanArchitecture.IntegrationTests/ApiEndpoints/PersonsApiTests.cs
@@ -7,42 +7,36 @@ namespace CleanArchitecture.IntegrationTests.ApiEndpoints;
 
 public class PersonsApiTests : IClassFixture<CustomWebApplicationFactory>
 {
-    private readonly HttpClient _client;
+    private readonly CustomWebApplicationFactory _factory;
 
     public PersonsApiTests(CustomWebApplicationFactory factory)
     {
-        _client = factory.CreateClient();
+        _factory = factory;
     }
 
-    [Fact]
-    public async Task GetHelloWorldMessage_ReturnsMessage()
+    [Theory]
+    [InlineData("b5d74ff1-572f-4dd5-beb3-3aa67adf6b49")]
+    [InlineData("53b2abec-a06b-4686-9c5a-5ede268eab6b")]
+    public async Task GetHelloWorldMessage_ReturnsSuccess(string personId)
     {
         // Arrange
+        var client = _factory.CreateClient();
 
         // Act
-        var response = await _client.GetAsync("/persons/b5d74ff1-572f-4dd5-beb3-3aa67adf6b49/message");
+        var response = await client.GetAsync($"/persons/{personId}/message");
         var message = await response.ProcessResponse<HelloWorldMessage>();
         
         // Assert
-        response.Should().HaveStatusCode(HttpStatusCode.OK);
-
+        response.Should().HaveStatusCode(HttpStatusCode.OK, "because the endpoint responded successfully");
         message.Should().NotBeNull("because the endpoint returns any message");
-        message.Message.Should().Be("Hello, Gerardo!", "because the person table returns 'Gerardo'");
-    }
-    
-    [Fact]
-    public async Task GetHelloWorldMessage_ReturnsDefaultMessage()
-    {
-        // Arrange
 
-        // Act
-        var response = await _client.GetAsync("/persons/53b2abec-a06b-4686-9c5a-5ede268eab6b/message");
-        var message = await response.ProcessResponse<HelloWorldMessage>();
-        
-        // Assert
-        response.Should().HaveStatusCode(HttpStatusCode.OK);
-
-        message.Should().NotBeNull("because the endpoint returns any message");
-        message.Message.Should().Be("Hello, World!", "because the person table returns no rows");
+        if (personId == "b5d74ff1-572f-4dd5-beb3-3aa67adf6b49")
+        {
+            message.Message.Should().Be("Hello, Gerardo!", "because the person table returns 'Gerardo'");
+        }
+        else
+        {
+            message.Message.Should().Be("Hello, World!", "because the person table returns no rows");
+        }
     }
 }
