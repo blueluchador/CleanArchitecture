668be3d3a3b32584ced4cba511aae34bc9c93f63
Gerry Gastelum <gerry.gastelum@xero.com>
2023-09-28 21:39:19 -0600

    Added persons service DI registration

--- src/CleanArchitecture.Application/Services/ServicesExtensions.cs
+++ src/CleanArchitecture.Application/Services/ServicesExtensions.cs
@@ -1,4 +1,5 @@
 using CleanArchitecture.Application.Contracts.Services;
+using CleanArchitecture.Application.DTOs;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CleanArchitecture.Application.Services;
@@ -8,6 +9,7 @@ public static class ServicesExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         return services
-            .AddScoped<IHelloWorldService, HelloWorldService>();
+            .AddScoped<IHelloWorldService, HelloWorldService>()
+            .AddScoped<IPersonsService, PersonsService>();
     }
 }
