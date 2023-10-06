98f2bad6d6ac55860f31db245653ecf309ce0195
Gerry Gastelum <gerry.gastelum@xero.com>
2023-10-06 11:21:21 -0600

    Created get person api tests

--- tests/CleanArchitecture.IntegrationTests/ApiEndpoints/PersonsApiTests.cs
+++ tests/CleanArchitecture.IntegrationTests/ApiEndpoints/PersonsApiTests.cs
@@ -17,7 +17,7 @@ public class PersonsApiTests : IClassFixture<CustomWebApplicationFactory>
     }
 
     [Fact]
-    public async Task GetPersons_Success()
+    public async Task GetPersons_ReturnsSuccess()
     {
         // Arrange
         var client = _factory.CreateClient();
@@ -32,17 +32,50 @@ public class PersonsApiTests : IClassFixture<CustomWebApplicationFactory>
         persons.Should().NotBeNull("because the endpoint never returns null");
         persons.Persons.Should().HaveCount(3, "because the endpoint responds with 3 Persons");
     }
+    
+    [Theory]
+    [InlineData("b5d74ff1-572f-4dd5-beb3-3aa67adf6b49", "Buck", "Russell")]
+    [InlineData("5ebeb2d5-80fb-4028-89c5-577ca4003ac5", "Austin", "Powers")]
+    [InlineData("d8b796f7-b2f1-4ccf-955c-bc5a9f6a6afd", "Rico", "Dynamite")]
+    public async Task GetPerson_ReturnsSuccess(string personId, string firstName, string lastName)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+    
+        // Act
+        var response = await client.GetAsync($"persons/{personId}");
+        var person = await response.ProcessResponse<Person>();
+        
+        // Assert
+        response.Should().HaveStatusCode(HttpStatusCode.OK, "because the endpoint responded successfully");
+        person.Should().NotBeNull("because the endpoint returns a Person object");
+
+        person.FirstName.Should().Be(firstName, $"because the Persons first name is '{firstName}'");
+        person.LastName.Should().Be(lastName, $"because the Persons last name is '{lastName}'");
+    }
+
+    [Fact]
+    public async Task GetPerson_ReturnsNotFound()
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+        
+        // Act
+        var response = await client.GetAsync("persons/53b2abec-a06b-4686-9c5a-5ede268eab6b");
+        
+        // Assert
+        response.Should().HaveStatusCode(HttpStatusCode.NotFound, "because the endpoint did not find a Person");
+    }
 
     [Theory]
     [InlineData("b5d74ff1-572f-4dd5-beb3-3aa67adf6b49", "Buck")]
     [InlineData("5ebeb2d5-80fb-4028-89c5-577ca4003ac5", "Austin")]
     [InlineData("d8b796f7-b2f1-4ccf-955c-bc5a9f6a6afd", "Rico")]
     [InlineData("53b2abec-a06b-4686-9c5a-5ede268eab6b", null)]
-    public async Task GetHelloWorldMessage_Success(string personId, string? name)
+    public async Task GetHelloWorldMessage_ReturnsSuccess(string personId, string? name)
     {
         // Arrange
         var client = _factory.CreateClient();
-        client.DefaultRequestHeaders.Add(ApiHeaders.TenantId, "ba5eba11-babe-505a-c0bb-dec1a551f1ed");
     
         // Act
         var response = await client.GetAsync($"persons/{personId}/message");
